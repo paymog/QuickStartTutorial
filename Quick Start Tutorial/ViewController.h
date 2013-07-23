c4ff6e176afa95188226aeca2ffa7fb1da65e2fd
Paymahn Moghadasian <paymahn1@gmail.com>
2013-07-22 20:33:15 -0500

    Finished Quick Start Tutorial (I suspect this tutorial is for using the Address Book)

--- Quick Start Tutorial/ViewController.h
+++ Quick Start Tutorial/ViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AddressBookUI/AddressBookUI.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <ABPeoplePickerNavigationControllerDelegate>
 
+
+@property (weak, nonatomic) IBOutlet UILabel *firstName;
+
+@property (weak, nonatomic) IBOutlet UILabel *phoneNumber;
+
+- (IBAction)showPicker:(id)sender;
 @end
--- Quick Start Tutorial/ViewController.m
+++ Quick Start Tutorial/ViewController.m
@@ -26,4 +26,51 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showPicker:(id)sender {
+    ABPeoplePickerNavigationController *picker = [[ABPeoplePickerNavigationController alloc] init];
+    
+    picker.peoplePickerDelegate = self;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person
+{
+    [self displayPerson:person];
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    return NO;
+}
+
+- (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier
+{
+    return NO;
+}
+
+- (void)displayPerson:(ABRecordRef)person
+{
+    NSString* name = (__bridge_transfer NSString*)ABRecordCopyValue(person, kABPersonFirstNameProperty);
+    
+    self.firstName.text = name;
+    
+    NSString* phone = nil;
+    ABMultiValueRef phoneNumbers = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    
+    if(ABMultiValueGetCount(phoneNumbers) > 0)
+    {
+        phone = (__bridge_transfer NSString*)ABMultiValueCopyValueAtIndex(phoneNumbers, 0);
+    }
+    else
+    {
+        phone = @"[None]";
+    }
+    
+    self.phoneNumber.text = phone;
+}
+
 @end
